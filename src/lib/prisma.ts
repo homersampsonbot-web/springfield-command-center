b5b5f175ebe9f33a1ec2ae56757df0067ecb5945
HomerBot <homersampsonbot@gmail.com>
2026-03-04 00:03:21 +0000

    fix: add prisma client lib

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,11 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    // @ts-ignore
-    adapter: undefined // We'll let Prisma 7 handle it if it can, otherwise we'll fix this
+    log: ["query"],
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
